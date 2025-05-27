2538744ef51056880e0abb7506a7ba919a4c9f53
Sephora13 <sephoradidavi6@gmail.com>
2025-05-27 11:32:39 +0100

    after tracking

--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import { SignalingService } from '../services/signaling.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-tracking',
@@ -6,6 +8,45 @@ import { Component } from '@angular/core';
   templateUrl: './tracking.component.html',
   styleUrl: './tracking.component.css'
 })
-export class TrackingComponent {
+export class TrackingComponent implements OnInit {
+  @ViewChild('remoteVideo') remoteVideo!: ElementRef<HTMLVideoElement>;
+  private peerConnection!: RTCPeerConnection;
+
+  constructor(private signaling: SignalingService, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    this.signaling.setRole('coordinateur');
+
+    this.peerConnection = new RTCPeerConnection({
+      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+    });
+
+    this.peerConnection.ontrack = (event) => {
+      console.log('[Coordinateur] Track received:', event.streams);
+      this.remoteVideo.nativeElement.srcObject = event.streams[0];
+    };
+
+    this.peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        console.log('[Coordinateur] Sending ICE candidate:', event.candidate);
+        this.signaling.sendCandidate(event.candidate);
+      }
+    };
+
+    this.signaling.offer$.subscribe(async offer => {
+      console.log('[Coordinateur] Offer received');
+      await this.peerConnection.setRemoteDescription(offer);
+      const answer = await this.peerConnection.createAnswer();
+      await this.peerConnection.setLocalDescription(answer);
+      this.signaling.sendAnswer(answer);
+    });
+
+    this.signaling.candidate$.subscribe(candidate => {
+      console.log('[Coordinateur] ICE candidate received:', candidate);
+      this.peerConnection.addIceCandidate(candidate);
+    });
+  }
 
 }
